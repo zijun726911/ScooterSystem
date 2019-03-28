f79af6c2a9c151b65444a88831249bee874fd99c
zijun726911 <zijun112233@163.com>
2019-03-28 15:21:24 +0800

    add entity package

---
+++ src/com/entity/Station.java
@@ -0,0 +1,12 @@
+package com.entity;
+
+import java.util.ArrayList;
+
+public class Station {
+	private int id;
+	private String name;
+	private int emptySlot;
+	private int totalSlot;
+	ArrayList<Slot> slots;
+	
+}
